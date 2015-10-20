86a9dff980dfadb7b48c8b8becfd0f5026193c83
jialinsun <jialin.sun@dianping.com>
2015-10-20 12:32:54 +0800

    recover hadoop code

--- cat-hadoop/src/main/java/com/dianping/cat/hadoop/hdfs/HarConnectionPool.java
+++ cat-hadoop/src/main/java/com/dianping/cat/hadoop/hdfs/HarConnectionPool.java
@@ -4,58 +4,120 @@ import java.io.IOException;
 import java.net.URI;
 import java.text.MessageFormat;
 import java.util.Date;
-import java.util.LinkedHashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.HarFileSystem;
+import org.codehaus.plexus.personality.plexus.lifecycle.phase.Initializable;
+import org.codehaus.plexus.personality.plexus.lifecycle.phase.InitializationException;
+import org.unidal.helper.Threads;
+import org.unidal.helper.Threads.Task;
+import org.unidal.tuple.Pair;
 
+import com.dianping.cat.Cat;
 import com.dianping.cat.config.server.ServerConfigManager;
 
-public class HarConnectionPool {
+public class HarConnectionPool implements Initializable {
 
 	private ServerConfigManager m_serverConfigManager;
 
-	private MessageFormat m_format = new MessageFormat("{0}/{1}/{2,date,yyyyMMdd}.har");
+	private MessageFormat m_format = new MessageFormat("{0}/{1}/{2,date,yyyyMMdd}/{2,date,HH}.har");
 
-	private Map<Date, HarFileSystem> m_hars = new LinkedHashMap<Date, HarFileSystem>() {
-
-		private static final long serialVersionUID = 1L;
-
-		@Override
-		protected boolean removeEldestEntry(Entry<Date, HarFileSystem> eldest) {
-			return size() > m_serverConfigManager.getHdfsMaxStorageTime();
-		}
-
-	};
+	private Map<String, Pair<HarFileSystem, Long>> m_hars = new ConcurrentHashMap<String, Pair<HarFileSystem, Long>>();
 
 	public HarConnectionPool(ServerConfigManager manager) {
 		m_serverConfigManager = manager;
 	}
 
+	private void closeIdleHarfs() throws IOException {
+		long now = System.currentTimeMillis();
+		long hour = 3600 * 1000L;
+		Set<String> closed = new HashSet<String>();
+
+		for (Entry<String, Pair<HarFileSystem, Long>> entry : m_hars.entrySet()) {
+			Pair<HarFileSystem, Long> pair = entry.getValue();
+
+			if (now - pair.getValue() >= hour) {
+				try {
+					pair.getKey().close();
+					closed.add(entry.getKey());
+				} catch (Exception e) {
+					Cat.logError(e);
+				}
+			}
+		}
+		for (String close : closed) {
+			m_hars.remove(close);
+		}
+	}
+
 	public HarFileSystem getHarfsConnection(String id, Date date, FileSystem fs) throws IOException {
-		HarFileSystem har = m_hars.get(date);
+		String serverUri = m_serverConfigManager.getHarfsServerUri(id);
+		String baseUri = m_serverConfigManager.getHarfsBaseDir(id);
+		String harUri = m_format.format(new Object[] { serverUri, baseUri, date });
+		Pair<HarFileSystem, Long> har = m_hars.get(harUri);
+		long current = System.currentTimeMillis();
 
 		if (har == null) {
 			synchronized (this) {
 				if (har == null) {
-					String serverUri = m_serverConfigManager.getHarfsServerUri(id);
-					String baseUri = m_serverConfigManager.getHarfsBaseDir(id);
-					String harUri = m_format.format(new Object[] { serverUri, baseUri, date });
+
 					URI uri = URI.create(harUri);
-					har = new HarFileSystem(fs);
+					HarFileSystem harfs = new HarFileSystem(fs);
 
 					try {
-						har.initialize(uri, har.getConf());
-						m_hars.put(date, har);
+						harfs.initialize(uri, harfs.getConf());
+						har = new Pair<HarFileSystem, Long>(harfs, current);
+
+						m_hars.put(harUri, har);
 					} catch (IOException e) {
 						// ignore
+						harfs.close();
 						return null;
 					}
 				}
 			}
 		}
-		return har;
+
+		har.setValue(current);
+		return har.getKey();
 	}
+
+	@Override
+	public void initialize() throws InitializationException {
+		Threads.forGroup("cat").start(new IdleChecker());
+	}
+
+	class IdleChecker implements Task {
+		@Override
+		public String getName() {
+			return "HarConnectionPool-IdleChecker";
+		}
+
+		@Override
+		public void run() {
+			try {
+				while (true) {
+					Thread.sleep(60 * 1000L); // 1 minute
+
+					try {
+						closeIdleHarfs();
+					} catch (IOException e) {
+						Cat.logError(e);
+					}
+				}
+			} catch (InterruptedException e) {
+				// ignore it
+			}
+		}
+
+		@Override
+		public void shutdown() {
+		}
+	}
+
 }
