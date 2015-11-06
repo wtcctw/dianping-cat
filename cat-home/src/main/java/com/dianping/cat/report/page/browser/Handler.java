f4eded51b126d5e59e1ddfc72d8c7716dcfbbd13
ccn <ccn1208@gmail.com>
2015-11-06 10:50:43 +0800

    add browser info

--- cat-home/src/main/java/com/dianping/cat/report/page/browser/Handler.java
+++ cat-home/src/main/java/com/dianping/cat/report/page/browser/Handler.java
@@ -155,6 +155,7 @@ public class Handler implements PageHandler<Context> {
 			model.setLevel(Level.getNameByCode(jsErrorLog.getLevel()));
 			model.setModule(jsErrorLog.getModule());
 			model.setDetail(new String(detail.getContent(), "UTF-8"));
+			model.setAgent(jsErrorLog.getBrowser());
 		} catch (Exception e) {
 			Cat.logError(e);
 		}
