89ab6ea763459555afcbebdd60385b4f67293921
jialinsun <jialin.sun@dianping.com>
2016-02-02 10:15:13 +0800

    async handle

--- cat-core/src/main/java/com/dianping/cat/task/ConfigSyncTask.java
+++ cat-core/src/main/java/com/dianping/cat/task/ConfigSyncTask.java
@@ -2,7 +2,9 @@ package com.dianping.cat.task;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
 
+import org.unidal.helper.Threads;
 import org.unidal.helper.Threads.Task;
 
 import com.dianping.cat.Cat;
@@ -11,15 +13,17 @@ import com.dianping.cat.message.Transaction;
 
 public class ConfigSyncTask implements Task {
 
-	private static final long DURATION = TimeHelper.ONE_MINUTE;
+	public static ConfigSyncTask getInstance() {
+		return m_instance;
+	}
 
 	private List<SyncHandler> m_handlers = new ArrayList<SyncHandler>();
 
 	private static ConfigSyncTask m_instance = new ConfigSyncTask();
 
-	public static ConfigSyncTask getInstance() {
-		return m_instance;
-	}
+	private static ExecutorService s_threadPool = Threads.forPool().getFixedThreadPool("Cat-ConfigSyncTask", 5);
+
+	private static final long DURATION = TimeHelper.ONE_MINUTE;
 
 	@Override
 	public String getName() {
@@ -39,18 +43,24 @@ public class ConfigSyncTask implements Task {
 		while (active) {
 			long current = System.currentTimeMillis();
 
-			for (SyncHandler handler : m_handlers) {
-				Transaction t = Cat.newTransaction("ConfigSynchronize", handler.getName());
+			for (final SyncHandler handler : m_handlers) {
+				s_threadPool.submit(new Runnable() {
 
-				try {
-					handler.handle();
-					t.setStatus(Transaction.SUCCESS);
-				} catch (Exception e) {
-					t.setStatus(e);
-					Cat.logError(e);
-				} finally {
-					t.complete();
-				}
+					@Override
+					public void run() {
+						final Transaction t = Cat.newTransaction("SyncConfig", handler.getName());
+
+						try {
+							handler.handle();
+							t.setStatus(Transaction.SUCCESS);
+						} catch (Exception e) {
+							t.setStatus(e);
+							Cat.logError(e);
+						} finally {
+							t.complete();
+						}
+					}
+				});
 			}
 
 			long duration = System.currentTimeMillis() - current;
