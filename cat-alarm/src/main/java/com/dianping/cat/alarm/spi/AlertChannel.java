eec6704172047faf925b07d4a8c30a095740f918
jialinsun <jialin.sun@dianping.com>
2016-02-16 17:27:06 +0800

    milestone before delete cathome alert module

---
+++ cat-alarm/src/main/java/com/dianping/cat/alarm/spi/AlertChannel.java
@@ -0,0 +1,31 @@
+package com.dianping.cat.alarm.spi;
+
+import com.dianping.cat.alarm.spi.AlertChannel;
+
+public enum AlertChannel {
+
+	MAIL("mail"),
+
+	SMS("sms"),
+
+	WEIXIN("weixin");
+
+	private String m_name;
+
+	public static AlertChannel findByName(String name) {
+		for (AlertChannel channel : values()) {
+			if (channel.getName().equals(name)) {
+				return channel;
+			}
+		}
+		return null;
+	}
+
+	private AlertChannel(String name) {
+		m_name = name;
+	}
+
+	public String getName() {
+		return m_name;
+	}
+}
