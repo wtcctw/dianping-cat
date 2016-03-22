fc7071e175a89bcf46177c28dfc8a8943c7b02af
jialinsun <jialin.sun@dianping.com>
2016-03-22 23:04:19 +0800

    for broker-service

--- cat-core/src/main/java/com/dianping/cat/config/app/AppCommandTableProvider.java
+++ cat-core/src/main/java/com/dianping/cat/config/app/AppCommandTableProvider.java
@@ -1,19 +1,13 @@
 package com.dianping.cat.config.app;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Map;
 
-import org.codehaus.plexus.personality.plexus.lifecycle.phase.Initializable;
-import org.codehaus.plexus.personality.plexus.lifecycle.phase.InitializationException;
 import org.unidal.dal.jdbc.QueryEngine;
 import org.unidal.dal.jdbc.mapping.TableProvider;
 
-import com.dianping.cat.Cat;
 import com.dianping.cat.app.AppCommandData;
 
-public class AppCommandTableProvider implements TableProvider, Initializable {
+public class AppCommandTableProvider implements TableProvider {
 
 	private String m_logicalTableName = "app-command-data";
 
@@ -21,17 +15,11 @@ public class AppCommandTableProvider implements TableProvider, Initializable {
 
 	private String m_dataSourceName = "app";
 
-	private Date m_historyDate;
-
 	@Override
 	public String getDataSourceName(Map<String, Object> hints) {
 		AppCommandData command = (AppCommandData) hints.get(QueryEngine.HINT_DATA_OBJECT);
 
-		if (command.getPeriod().before(m_historyDate)) {
-			return m_dataSourceName;
-		} else {
-			return m_dataSourceName + "_" + command.getCommandId() % 5;
-		}
+		return m_dataSourceName + "_" + command.getCommandId() % 5;
 	}
 
 	@Override
@@ -52,17 +40,6 @@ public class AppCommandTableProvider implements TableProvider, Initializable {
 
 	public void setLogicalTableName(String logicalTableName) {
 		m_logicalTableName = logicalTableName;
-	}
-
-	@Override
-	public void initialize() throws InitializationException {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-
-		try {
-			m_historyDate = sdf.parse("2016-03-22 21:00");
-		} catch (ParseException e) {
-			Cat.logError(e);
-		}
 	}
 
 }
