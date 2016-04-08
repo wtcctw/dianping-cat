70df4c2ee1c860fd0e596aec85dbf92decbb8f70
ccn <ccn1208@gmail.com>
2016-04-08 11:06:12 +0800

    modify alert mail

--- cat-home/src/main/java/com/dianping/cat/report/alert/business2/BusinessDecorator2.java
+++ cat-home/src/main/java/com/dianping/cat/report/alert/business2/BusinessDecorator2.java
@@ -1,18 +1,38 @@
 package com.dianping.cat.report.alert.business2;
 
+import java.util.Calendar;
+import java.util.Date;
+
+import org.unidal.lookup.annotation.Inject;
+
 import com.dianping.cat.alarm.spi.AlertEntity;
 import com.dianping.cat.alarm.spi.AlertType;
 import com.dianping.cat.alarm.spi.decorator.ProjectDecorator;
+import com.dianping.cat.report.alert.summary.AlertSummaryExecutor;
 
 public class BusinessDecorator2 extends ProjectDecorator {
 
 	public static final String ID = AlertType.Business2.getName();
-
+	
+	@Inject
+	private AlertSummaryExecutor m_executor;
+	
 	@Override
 	public String generateContent(AlertEntity alert) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(alert.getDate());
+		cal.set(Calendar.SECOND, 0);
+		cal.set(Calendar.MILLISECOND, 0);
+		Date alertDate = cal.getTime();
+		
 		StringBuilder sb = new StringBuilder();
 		sb.append(alert.getContent());
 		sb.append(buildContactInfo(alert.getDomain()));
+		
+		String summaryContext = m_executor.execute(alert.getDomain(), alertDate);
+		if (summaryContext != null) {
+			sb.append("<br/>").append(summaryContext);
+		}
 
 		return sb.toString();
 	}
