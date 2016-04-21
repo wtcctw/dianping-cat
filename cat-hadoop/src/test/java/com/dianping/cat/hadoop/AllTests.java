3ac8531ddfa1289cfa5963e1275f3c90423f3e7d
youyong205 <youyong205@126.com>
2016-04-21 11:28:18 +0800

    modify the test

--- cat-hadoop/src/test/java/com/dianping/cat/hadoop/AllTests.java
+++ cat-hadoop/src/test/java/com/dianping/cat/hadoop/AllTests.java
@@ -3,9 +3,25 @@ package com.dianping.cat.hadoop;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+import org.unidal.cat.message.BenchmarkTest;
+import org.unidal.cat.message.MessageIdTest;
+import org.unidal.cat.message.storage.IndexManagerTest;
+import org.unidal.cat.message.storage.IndexTest;
+import org.unidal.cat.message.storage.TokenMappingTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({
+	MessageIdTest.class,
+
+	BenchmarkTest.class,
+
+	IndexManagerTest.class,
+
+	IndexTest.class,
+
+	TokenMappingTest.class,
+
+	IndexTest.class,
 
 })
 public class AllTests {
