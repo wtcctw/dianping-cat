e08b89598627f1f4dfef72b101d8016bdc15da0b
youyong205 <youyong205@126.com>
2016-04-24 16:46:37 +0800

    add the test

--- cat-hadoop/src/test/java/org/unidal/cat/message/storage/CompressPerformanceTest.java
+++ cat-hadoop/src/test/java/org/unidal/cat/message/storage/CompressPerformanceTest.java
@@ -16,6 +16,8 @@ import com.dianping.cat.message.spi.codec.PlainTextMessageCodec;
 
 public class CompressPerformanceTest extends ComponentTestCase {
 	private MessageCodec m_codec;
+	
+	private int m_total = 200000;
 
 	@Before
 	public void before() {
@@ -37,7 +39,7 @@ public class CompressPerformanceTest extends ComponentTestCase {
 
 		long start = System.currentTimeMillis();
 		long size = 0;
-		for (int i = 0; i < 20000; i++) {
+		for (int i = 0; i < m_total; i++) {
 			Block block = new DefaultBlock(domain, hour, CompressTye.GZIP);
 
 			for (int index = 0; index < 500; index++) {
@@ -61,7 +63,7 @@ public class CompressPerformanceTest extends ComponentTestCase {
 
 		long start = System.currentTimeMillis();
 		long size = 0;
-		for (int i = 0; i < 20000; i++) {
+		for (int i = 0; i < m_total; i++) {
 			Block block = new DefaultBlock(domain, hour, CompressTye.DEFLATE);
 
 			for (int index = 0; index < 500; index++) {
@@ -86,7 +88,7 @@ public class CompressPerformanceTest extends ComponentTestCase {
 
 		long start = System.currentTimeMillis();
 		long size = 0;
-		for (int i = 0; i < 20000; i++) {
+		for (int i = 0; i < m_total; i++) {
 			Block block = new DefaultBlock(domain, hour, CompressTye.SNAPPY);
 
 			for (int index = 0; index < 500; index++) {
