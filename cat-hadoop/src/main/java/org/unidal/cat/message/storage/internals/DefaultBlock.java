dee7711b0f97f88da5da10d69ede4bfb058647b1
youyong205 <youyong205@126.com>
2016-04-26 10:34:43 +0800

    fix in memory find message bug

--- cat-hadoop/src/main/java/org/unidal/cat/message/storage/internals/DefaultBlock.java
+++ cat-hadoop/src/main/java/org/unidal/cat/message/storage/internals/DefaultBlock.java
@@ -26,7 +26,7 @@ import com.dianping.cat.Cat;
 import com.dianping.cat.message.internal.MessageId;
 
 public class DefaultBlock implements Block {
-	
+
 	private String m_domain;
 
 	private int m_hour;
@@ -42,13 +42,13 @@ public class DefaultBlock implements Block {
 	private OutputStream m_out;
 
 	private boolean m_isFulsh;
-	
+
 	private static final int BUFFER_SIZE = 1024;
 
 	public static int MAX_SIZE = 256 * 1024;
 
 	public static CompressTye COMMPRESS_TYPE = CompressTye.SNAPPY;
-	
+
 	public static int DEFLATE_LEVEL = 5;
 
 	public DefaultBlock(MessageId id, int offset, byte[] data) {
@@ -127,8 +127,7 @@ public class DefaultBlock implements Block {
 
 			try {
 				ByteBuf copyData = Unpooled.copiedBuffer(m_data);
-				@SuppressWarnings("resource")
-				DataInputStream in = new DataInputStream(new GZIPInputStream(new ByteBufInputStream(copyData)));
+				DataInputStream in = new DataInputStream(createInputSteam(copyData, COMMPRESS_TYPE));
 
 				in.skip(offset);
 				int length = in.readInt();
